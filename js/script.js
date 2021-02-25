34d226cd45506a6303f31a170b678f1a46371430
sowunmi mayowa <smayowa176@gmail.com>
2021-02-24 20:42:38 -0800

    validated contact form

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 const menuBtn = document.querySelector(".menu-btn");
 const navs = document.querySelector(".navigation");
+const userName = document.querySelector("#username");
+const userEmail = document.querySelector("#useremail");
+const comment = document.querySelector("#comment");
+const contactForm = document.querySelector("form");
 
 menuBtn.addEventListener("click", showNav);
 
@@ -12,4 +16,25 @@ function showNav(){
     else{
         navs.style.left = "-250px"
     }
+}
+
+function borderDanger() {
+    userName.classList.add('danger')
+}
+contactForm.addEventListener('submit', validateForm);
+
+function validateForm(e) {
+    e.preventDefault();
+
+    if (userName.value == "" || userEmail.value == "" || comment.value == "") {
+        let errorMsg = document.createElement('div');
+        errorMsg.textContent = "all fields are required";
+        errorMsg.classList.add("error")
+        document.querySelector(".msgelem").appendChild(errorMsg);
+
+        setTimeout(() => {
+            errorMsg.remove()
+        }, 3000);
+    }
+    borderDanger();
 }
